3dc0176987060fb564586967e75857dec932c10d
frank3stein <martinov.emil@gmail.com>
2021-10-12 15:49:24 +0200

    Project start

---
+++ src/database.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv'
+import {Pool} from 'pg'
+
+dotenv.config()
+
+// const POSTGRES_HOST = '127.0.0.1'
+// const POSTGRES_DB = 'storefront'
+// const POSTGRES_USER = 'storefront_backend'
+// const POSTGRES_PASSWORD = 'password'
+const {POSTGRES_DB, POSTGRES_HOST, POSTGRES_PASSWORD, POSTGRES_USER} = process.env;
+
+
+const client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD
+})
+
+export default client;
